548bca601f81783396ce46e1241e35ad088f7bbd
Edo Hooghiemster <0931317@hr.nl>
2019-05-21 10:04:49 +0200

    AceCardApp read feature

--- test/AwesomeProject/App.js
+++ test/AwesomeProject/App.js
@@ -1,3 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
@@ -8,10 +16,8 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component {
-  constructor(props){
-    super(props)
-  }
+type Props = {};
+export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
